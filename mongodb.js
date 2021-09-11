3405219e51d1d9f78d18b6dc5256edef2dbbd844
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-09-11 20:02:40 +0700

    About mongoDB FindOne and Find

--- mongodb.js
+++ mongodb.js
@@ -8,17 +8,62 @@ const {MongoClient, ObjectId} = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectId()
-console.log(id.id.length)
-console.log(id.getTimestamp())
-console.log(id.toHexString())
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => {
     if (error) {
         return console.log('Unable to connct to database: ' + error)
     }
     
     const db = client.db(databaseName)
+    // ------------------------Find one user------------------------
+    // db.collection('users').findOne({
+    //     _id: new ObjectId('613ca26eb35555b6b349168b')
+    // }, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to find user: ' + error)
+    //     }
+    //     console.log(user)
+    // })
+
+    // ------------------------Find many users------------------------
+    // db.collection('users').find({
+    //      age: 22
+    // }).toArray((error, users) => {
+    //     if (error) {
+    //         return console.log('Unable to find user: ' + error)
+    //     }
+    //     console.log(users)
+    // })
+
+    // ------------------------Count many users------------------------
+    // db.collection('users').find({
+    //     age: 22
+    // }).count((error, count) => {
+    //     if (error) {
+    //         return console.log('Unable to count user: ' + error)
+    //     }
+    //     console.log(count)
+    // })
+
+    // ------------------------Find one task------------------------
+    db.collection('tasks').findOne({
+        _id: new ObjectId('613c906af1cce609a5e83e35')
+    }, (error, result) => {
+        if (error) {
+            return console.log('Unable to find task: ' + error)
+        }
+        console.log(result)
+    })
+
+    // ------------------------Find many tasks------------------------
+    db.collection('tasks').find().toArray((error, result) => {
+        if (error) {
+            return console.log('Unable to find task: ' + error)
+        }
+        console.log(result)
+    })
+
+
+    // ------------------------INSERT------------------------------------------------
 
     // --------------------------insert one user collection--------------------------
     // db.collection('users').insertOne({
@@ -50,7 +95,7 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => 
     // })
 
     // --------------------------insert many task collection--------------------------
-    // db.collection('task').insertMany([
+    // db.collection('tasks').insertMany([
     //     { 
     //         description: 'Clean the house', 
     //         complete: true
