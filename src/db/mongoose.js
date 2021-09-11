ab72f3bd5fa5556cf107fd7b34af1582e9d46859
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-09-12 03:46:56 +0700

    Add get/post endpoint for users/tasks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,14 +3,3 @@ const validator = require('validator')
 
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api'
 mongoose.connect(connectionURL)
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-    },
-    completed: {
-        type: Boolean,
-        default: false,
-    }
-})
