a0f5b9563e9994d96a4b35ad85b6ab728a52a9ab
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-09-11 17:52:14 +0700

    Connect mongoDb

--- mongodb.js
+++ mongodb.js
@@ -1,1 +1,19 @@
 // CRUD create read update dalete
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => {
+    if (error) {
+        return console.log('Unable to connct to database: ' + error)
+    }
+    
+    const db = client.db(databaseName)
+    db.collection('users').insertOne({
+        name: 'Theerapon',
+        age: 22
+    })
+})
