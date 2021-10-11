31cfc4a188f1ecb21689be26705203dfaf5c98f3
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-10-11 16:24:28 +0700

    About read, delete all, one task with auth

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -49,6 +50,12 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner',
+})
+
 userSchema.methods.toJSON = function () {
     const user = this
     const userObject = user.toObject()
@@ -90,6 +97,14 @@ userSchema.pre('save', async function (next) {
 
     next()
 })
+
+// Delete user tasks when user is reomved
+userSchema.pre('remove', async function (next) {
+    const user = this
+    await Task.deleteMany({ owner: user.id })
+    next()
+})
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = User
